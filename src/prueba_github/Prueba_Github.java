4b6ad41b935e63753e1a7ffd8382ae8f43e54353
daw126 <daw126@35-11.aglinformatica.es>
2021-04-15 12:54:50 +0200

    Agregadas 2 líneas

--- src/prueba_github/Prueba_Github.java
+++ src/prueba_github/Prueba_Github.java
@@ -17,6 +17,8 @@ public class Prueba_Github {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola");
+        System.out.println("1");
+        System.out.println("2");
     }
     
 }
