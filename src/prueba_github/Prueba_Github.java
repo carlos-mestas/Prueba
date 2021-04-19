9d3646789d1f9df67944079b62313a455d01766c
lmolero <lisanymoleromachado@gmail.com>
2021-04-19 12:26:48 +0200

    He eliminado una linea

--- src/prueba_github/Prueba_Github.java
+++ src/prueba_github/Prueba_Github.java
@@ -17,7 +17,6 @@ public class Prueba_Github {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola");
-        System.out.println("1");
         System.out.println("2");
         //Version 1
         System.out.println("Hola, soy Lisany! estoy editando tu código");
